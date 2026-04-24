857a1eb8670708ec901e529e75dfd7348d687e95
MarGreenDev <marylougroeneveld@gmail.com>
2026-04-24 10:55:43 +0200

    Revamp home view layout and styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,39 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
-<body class="p-6">
+<body class="p-0 bg-green-50">
 
-    <div class="border p-6 rounded-lg shadow w-64">
-        <h1 class="text-x1 font-bold">{{$name}}</h1>
+    <header class="bg-green-400">
+        <h1 class="text-5xl font-mono tracking-widest text-center">{{$name}} B)</h1>
+        <nav>
+            <ul class="flex gap-5 justify-center">
+                <li>Contact</li>
+                <li>FAQ</li>
+                <li>Cool</li>
+            </ul>
+        </nav>
+    </header>
 
-        <p class="text-gray-500 mt-4">
-            {{$price}}
-        </p>
+    <main class="p-6">
 
-        <a href="/order">
-            <button class="bg-blue-500 text-white px-4 py-2 mt-4 rounded cursor-pointer">
-                Order
-            </button>
-        </a>
-    </div>
+        <section class="p-4 flex flex-col items-center border w-fit justify-self-center hover:shadow-xl transition-transform duration-200 hover:scale-105 hover:translate-y-1">
+            <h2 class="text-xl text-center">Version 1.0</h2>
+            <img class="size-100" src="https://placehold.co/400" alt="placeholder">
+            <p>Yes very cool</p>
+            <a href="/order">
+                <button class="bg-green-400 cursor-pointer text-white p-3">Pre-order</button>
+            </a>
+        </section>
+
+        <section>
+            <h2 class="text-xl font-mono">Features</h2>
+            <ul class="list-disc">
+                <li>bluetooth v67</li>
+                <li>unlimited range</li>
+            </ul>
+        </section>
+
+    </main>
 
 </body>
 
